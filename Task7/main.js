3088c25813d5782016856f61129ee680f8a06bc3
hamzee426 <hamzee426@gmail.com>
2024-02-17 19:35:33 +0500

    Task 7 Done

---
+++ Task7/main.js
@@ -0,0 +1,8 @@
+var add = 2 + 6;
+var sub = 10 - 2;
+var mult = 4 * 2;
+var div = 16 / 2;
+console.log("Addition       : ", add);
+console.log("Subtraction    : ", sub);
+console.log("Multiplication : ", mult);
+console.log("Division       : ", div);
---
+++ Task7/main.ts
@@ -0,0 +1,10 @@
+let add:number=2+6;
+let sub:number=10-2;
+let mult:number=4*2;
+let div:number=16/2;
+
+console.log("Addition       : ",add);
+console.log("Subtraction    : ",sub);
+console.log("Multiplication : ",mult);
+console.log("Division       : ",div);
+
