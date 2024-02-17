ad6e08464d5b8a003153a10bea6e8c9628f94093
hamzee426 <hamzee426@gmail.com>
2024-02-17 18:53:58 +0500

    Task 3 Done

---
+++ Task3/main.js
@@ -0,0 +1,4 @@
+var myName = "hamza";
+console.log("Lowercase : " + myName.toLowerCase());
+console.log("Uppercase : " + myName.toUpperCase());
+console.log("Title Case : " + myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase());
---
+++ Task3/main.ts
@@ -0,0 +1,6 @@
+let myName: string="hamza";
+
+console.log("Lowercase : "+myName.toLowerCase());
+console.log("Uppercase : "+myName.toUpperCase());
+console.log("Title Case : "+myName.charAt(0).toUpperCase()+myName.slice(1).toLowerCase());
+
