5163726bfa9b3238d99dfb84d39e305fc6b53182
hamzee426 <hamzee426@gmail.com>
2024-02-17 18:57:50 +0500

    Task 5 Done

---
+++ Task5/main.js
@@ -0,0 +1,2 @@
+var myAuthor = "Albert Einstein";
+console.log("".concat(myAuthor, " once said, \u201CA person who never made a mistake never tried anything new.\u201D"));
---
+++ Task5/main.ts
@@ -0,0 +1,3 @@
+let myAuthor:string="Albert Einstein";
+
+console.log(`${myAuthor} once said, “A person who never made a mistake never tried anything new.”`);
