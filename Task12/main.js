1b5e9f306117a6b2f016d183d855e5f5a768163c
hamzee426 <hamzee426@gmail.com>
2024-02-18 07:33:09 +0500

    Task 12 Done

---
+++ Task12/main.js
@@ -0,0 +1,4 @@
+var names = ["Hamza", "Ali", "Hassan"];
+names.forEach(function (element) {
+    console.log("Hi, ", element);
+});
---
+++ Task12/main.ts
@@ -0,0 +1,6 @@
+const names:string[]=["Hamza","Ali","Hassan"];
+
+names.forEach(element=>{
+    console.log("Hi, ",element);
+    
+});
