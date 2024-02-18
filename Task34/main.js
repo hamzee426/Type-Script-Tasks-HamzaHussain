4a251ac96883f74b85374f7724d1b39922faea38
hamzee426 <hamzee426@gmail.com>
2024-02-18 12:00:32 +0500

    Task 34 Done

---
+++ Task34/main.js
@@ -0,0 +1,12 @@
+var favorite_pizzas = ["Pepperoni", "Margherita", "BBQ Chicken"];
+console.log("Favorite Pizzas:");
+for (var _i = 0, favorite_pizzas_1 = favorite_pizzas; _i < favorite_pizzas_1.length; _i++) {
+    var pizza = favorite_pizzas_1[_i];
+    console.log(pizza);
+}
+console.log("\nStatements about Favorite Pizzas:");
+for (var _a = 0, favorite_pizzas_2 = favorite_pizzas; _a < favorite_pizzas_2.length; _a++) {
+    var pizza = favorite_pizzas_2[_a];
+    console.log("I like ".concat(pizza, " pizza."));
+}
+console.log("\nI really love pizza!");
---
+++ Task34/main.ts
@@ -0,0 +1,13 @@
+let favorite_pizzas: string[] = ["Pepperoni", "Margherita", "BBQ Chicken"];
+
+console.log("Favorite Pizzas:");
+for (let pizza of favorite_pizzas) {
+    console.log(pizza);
+}
+
+console.log("\nStatements about Favorite Pizzas:");
+for (let pizza of favorite_pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+
+console.log("\nI really love pizza!");
