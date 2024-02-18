cbe6c0e53186fa833d382fee558d279b9b25153e
hamzee426 <hamzee426@gmail.com>
2024-02-18 14:39:38 +0500

    Task 44 Done

--- Task44/main.js
+++ Task44/main.js
@@ -1,14 +1,17 @@
-function order_sandwich(...items: string[]): void {
+function order_sandwich() {
+    var items = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        items[_i] = arguments[_i];
+    }
     console.log("Sandwich Summary:");
     if (items.length === 0) {
         console.log("No items selected. Please choose some items for your sandwich.");
-    } else {
+    }
+    else {
         console.log("Items selected: " + items.join(", "));
     }
     console.log();
 }
-
-
 order_sandwich("Bread", "Cheese", "Turkey");
 order_sandwich("Bread", "Ham", "Lettuce", "Tomato");
-order_sandwich(); 
+order_sandwich();
