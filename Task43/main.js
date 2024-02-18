323b42c5c7977cb2a7eeecdc14cbc42457bc6b41
hamzee426 <hamzee426@gmail.com>
2024-02-18 14:38:20 +0500

    Task 43 Done

---
+++ Task43/main.js
@@ -0,0 +1,24 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+function show_magicians(magicians) {
+    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
+        var magician = magicians_1[_i];
+        console.log(magician);
+    }
+}
+function make_great(magicians) {
+    return magicians.map(function (magician) { return magician + " the Great"; });
+}
+var magicians = ["David Copperfield", "Houdini", "Derren Brown", "Penn and Teller"];
+var great_magicians = make_great(__spreadArray([], magicians, true));
+console.log("Original Magicians:");
+show_magicians(magicians);
+console.log("\nGreat Magicians:");
+show_magicians(great_magicians);
---
+++ Task43/main.ts
@@ -0,0 +1,22 @@
+function show_magicians(magicians: string[]): void {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+
+function make_great(magicians: string[]): string[] {
+    return magicians.map(magician => magician + " the Great");
+}
+
+
+let magicians: string[] = ["David Copperfield", "Houdini", "Derren Brown", "Penn and Teller"];
+
+
+let great_magicians = make_great([...magicians]);
+
+
+console.log("Original Magicians:");
+show_magicians(magicians);
+
+console.log("\nGreat Magicians:");
+show_magicians(great_magicians);
