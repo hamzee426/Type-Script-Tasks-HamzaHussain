643f0dc431f968f890ceb318c8bbf3d27732427d
hamzee426 <hamzee426@gmail.com>
2024-02-18 07:10:50 +0500

    Task 10 Done

---
+++ Task10/main.js
@@ -0,0 +1,7 @@
+//This is Task2 and it is printing my name.
+console.log("Hi, I am Hamza");
+//This is task 3 and it shows String to lowercase, uppercase and title case
+var myName = "hamza";
+console.log("Lowercase : " + myName.toLowerCase());
+console.log("Uppercase : " + myName.toUpperCase());
+console.log("Title Case : " + myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase());
---
+++ Task10/main.ts
@@ -0,0 +1,10 @@
+//This is Task2 and it is printing my name.
+console.log("Hi, I am Hamza");
+
+//This is task 3 and it shows String to lowercase, uppercase and title case
+let myName: string="hamza";
+
+console.log("Lowercase : "+myName.toLowerCase());
+console.log("Uppercase : "+myName.toUpperCase());
+console.log("Title Case : "+myName.charAt(0).toUpperCase()+myName.slice(1).toLowerCase());
+
