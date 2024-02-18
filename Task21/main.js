45322e783ad15ae3241fa312cc075a8e93ec3a52
hamzee426 <hamzee426@gmail.com>
2024-02-18 10:49:54 +0500

    Task 21 Done

--- Task21/main.js
+++ Task21/main.js
@@ -1,13 +1,11 @@
-const cars:{ brand:string, model:string, year:number}[]=[
+var cars = [
     { brand: "Toyota", model: "Camry", year: 2022 },
     { brand: "Honda", model: "Civic", year: 2021 },
     { brand: "Ford", model: "Mustang", year: 2023 },
     { brand: "Tesla", model: "Model 3", year: 2022 },
     { brand: "BMW", model: "X5", year: 2020 }
-]
-
+];
 console.log("List of Cars");
-
-cars.forEach(cars=>{
-    console.log(`Brand : ${cars.brand} Model : ${cars.model} Year : ${cars.year}`);
+cars.forEach(function (cars) {
+    console.log("Brand : ".concat(cars.brand, " Model : ").concat(cars.model, " Year : ").concat(cars.year));
 });
