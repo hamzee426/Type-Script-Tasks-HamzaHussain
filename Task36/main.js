0785fab116dc09f6c445c6f32095c303944ac832
hamzee426 <hamzee426@gmail.com>
2024-02-18 14:25:46 +0500

    Task 36 Done

---
+++ Task36/main.js
@@ -0,0 +1,4 @@
+function make_shirt(size, message) {
+    console.log("Summary: Size - ".concat(size, ", Message - \"").concat(message, "\""));
+}
+make_shirt("Medium", "Hello, World!");
---
+++ Task36/main.ts
@@ -0,0 +1,5 @@
+function make_shirt(size: string, message: string): void {
+    console.log(`Summary: Size - ${size}, Message - "${message}"`);
+}
+
+make_shirt("Medium", "Hello, World!");
