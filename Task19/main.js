00697eb9780d888e7fe973949a433bf132920652
hamzee426 <hamzee426@gmail.com>
2024-02-18 10:34:14 +0500

    Task 19 Done

---
+++ Task19/main.js
@@ -0,0 +1,2 @@
+var placesToVisit = ["Tokyo", "Paris", "Machu Picchu", "New York City", "Great Barrier Reef"];
+console.log("Number of elements in array are : ", placesToVisit.length);
---
+++ Task19/main.ts
@@ -0,0 +1,3 @@
+const placesToVisit: string[] = ["Tokyo", "Paris", "Machu Picchu", "New York City", "Great Barrier Reef"];
+
+console.log("Number of elements in array are : ",placesToVisit.length);
