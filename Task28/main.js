3edcb1e45ddbfd7145ffabf6cba1664c97992c15
hamzee426 <hamzee426@gmail.com>
2024-02-18 11:46:04 +0500

    Task 28 Done

--- Task28/main.js
+++ Task28/main.js
@@ -1,15 +1,19 @@
-let age: number = 25;
-
+var age = 25;
 if (age < 2) {
     console.log("The person is a baby.");
-} else if (age >= 2 && age < 4) {
+}
+else if (age >= 2 && age < 4) {
     console.log("The person is a toddler.");
-} else if (age >= 4 && age < 13) {
+}
+else if (age >= 4 && age < 13) {
     console.log("The person is a kid.");
-} else if (age >= 13 && age < 20) {
+}
+else if (age >= 13 && age < 20) {
     console.log("The person is a teenager.");
-} else if (age >= 20 && age < 65) {
+}
+else if (age >= 20 && age < 65) {
     console.log("The person is an adult.");
-} else {
+}
+else {
     console.log("The person is an elder.");
 }
