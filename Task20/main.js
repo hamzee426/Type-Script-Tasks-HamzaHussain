4dbfc644e3203452b7b7116482d0e634f7f21a62
hamzee426 <hamzee426@gmail.com>
2024-02-18 10:36:36 +0500

    Task 20 Done

---
+++ Task20/main.js
@@ -0,0 +1,5 @@
+var fruits = ["Apple", "Banana", "Orange", "Grapes", "Mango"];
+console.log("List of Fruits:");
+fruits.forEach(function (fruit) {
+    console.log(fruit);
+});
---
+++ Task20/main.ts
@@ -0,0 +1,6 @@
+const fruits: string[] = ["Apple", "Banana", "Orange", "Grapes", "Mango"];
+
+console.log("List of Fruits:");
+fruits.forEach(fruit => {
+    console.log(fruit);
+});
