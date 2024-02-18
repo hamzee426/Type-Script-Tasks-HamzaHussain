a2c0a68c6899c77efd464b89a7992657a00ef98b
hamzee426 <hamzee426@gmail.com>
2024-02-18 07:43:34 +0500

    Task 14

---
+++ Task14/main.js
@@ -0,0 +1,4 @@
+var list = ["Hamza", "Ali", "Hassan"];
+list.forEach(function (list) {
+    console.log("Hi ".concat(list, ", You are invited for Dinner."));
+});
---
+++ Task14/main.ts
@@ -0,0 +1,5 @@
+const list:string[]=["Hamza","Ali","Hassan"];
+
+list.forEach(list=>{
+    console.log(`Hi ${list}, You are invited for Dinner.`);
+});
