6ba3ec2bdfeeec81341e6fb01ebb1fba1d2de30c
hamzee426 <hamzee426@gmail.com>
2024-02-18 14:33:21 +0500

    Task 41 Done

---
+++ Task41/main.ts
@@ -0,0 +1,11 @@
+function show_magicians(magicians: string[]): void {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+
+
+let magicians: string[] = ["David Copperfield", "Houdini", "Derren Brown", "Penn and Teller"];
+
+
+show_magicians(magicians);
