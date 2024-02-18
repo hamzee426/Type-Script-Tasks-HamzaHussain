6be6f758da5223267bd33ea8a4a7f77544e3752d
hamzee426 <hamzee426@gmail.com>
2024-02-18 11:36:31 +0500

    Task 25 Done

--- Task25/main.js
+++ Task25/main.js
@@ -1,15 +1,14 @@
-let alien_color: string = 'green';
-
+var alien_color = 'green';
 if (alien_color === 'green') {
     console.log("Congratulations! You just earned 5 points.");
-} else {
+}
+else {
     console.log("No points earned this time.");
 }
-
 alien_color = 'red';
-
 if (alien_color === 'green') {
     console.log("Congratulations! You just earned 5 points.");
-} else {
+}
+else {
     console.log("No points earned this time.");
 }
